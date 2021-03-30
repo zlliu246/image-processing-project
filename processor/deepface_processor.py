f19f893e7724b9b194e7b444ca2ffe0b8586c1ca
Leong Hock Ho <lhho.2017@sis.smu.edu.sg>
2021-03-30 22:16:03 +0800

    updated face detector

--- processor/deepface_processor.py
+++ processor/deepface_processor.py
@@ -5,12 +5,13 @@ from PIL import Image, ImageDraw
 from deepface.commons.functions import initialize_detector, load_image, detect_face2
 from deepface import DeepFace
 import processor.reid_processor as reid
+import time
 
 detector_backend = 'mtcnn'
 grayscale = False
 enforce_detection = False
 return_region = False
-db_folder = "db"
+db_folder = "db_face"
 col_dict = {"ZuoLin" : "Green", "JiaPeng": "Red", "Cheng": "Blue"}
 
 
@@ -29,9 +30,10 @@ def detect_deepface(img):
         
         try:
             new_im = Image.fromarray(detected_face)
-            new_im.save("current.jpg")
-            df = DeepFace.find(img_path = "current.jpg", db_path = "db_face", enforce_detection=False)
-            df.sort_values('VGG-Face_cosine', inplace=True, ascending=True)
+            tmp_image = str(int(time.time())) + ".jpg"
+            new_im.save(tmp_image)
+            df = DeepFace.find(img_path = tmp_image, db_path = db_folder, enforce_detection=False)
+            os.remove(tmp_image)
             face_detected = re.split(r' |/|\\', df['identity'].iloc[0])[1]
         except Exception as err:
             print("ERROR:", err)
@@ -72,12 +74,18 @@ def detect_deepface_cropped(img, person_boxes):
             best_body_guess, body_confidence = reid.detect_body_cropped(cropped_img)
             
             new_im = Image.fromarray(detected_face)
-            new_im.save("current.jpg")
-            df = DeepFace.find(img_path = "current.jpg", db_path = "db_face", enforce_detection=False)
+            tmp_image = str(int(time.time())) + ".jpg"
+            new_im.save(tmp_image)
+            df = DeepFace.find(img_path = tmp_image, db_path = db_folder, enforce_detection=False)
+            os.remove(tmp_image)
             df.sort_values('VGG-Face_cosine', inplace=True, ascending=True)
             face_detected = re.split(r' |/|\\', df['identity'].iloc[0])[1]
+            dist = float(df['VGG-Face_cosine'].iloc[0])
             
             ## need code when face id is not confident, use reid to confirm/dispute
+            if dist > 0.025:
+                print("face id is not confident")
+
         except Exception as err:
             print("ERROR:", err)
 
